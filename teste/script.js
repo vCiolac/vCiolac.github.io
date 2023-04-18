e13f273affd9ba089d6b474e3cf80676fb8c5dd0
Victor Ciolac <victor.bcfraga@gmail.com>
2023-04-18 14:25:48 -0300

    Configurando o localStorage

--- teste/script.js
+++ teste/script.js
@@ -5,31 +5,41 @@ const actionControls = document.getElementsByClassName('actionControls')[0];
 const pageButton = document.getElementsByClassName('pageButton');
 const actionButton = document.getElementsByClassName('actionButton');
 
-const generateButtons = () => {
-const button1 = document.createElement('button');
-const button2 = document.createElement('button');
-const button3 = document.createElement('button');
-const button4 = document.createElement('button');
-button1.id = 'prevPage';
-button1.classList.add('pageButton');
-button1.innerHTML = 'Voltar';
-button2.id = 'nextPage';
-button2.classList.add('pageButton');
-button2.innerHTML = 'Avançar';
-button3.id = 'firstAction';
-button3.classList.add('actionButton');
-button3.innerHTML = 'Bater';
-button4.id = 'secondAction';
-button4.classList.add('actionButton');
-button4.innerHTML = 'Defender';
-pageControls.appendChild(button1);
-pageControls.appendChild(button2);
-actionControls.appendChild(button3);
-actionControls.appendChild(button4);
-}
+const generatePageButtons = () => {
+  const button1 = document.createElement('button');
+  const button2 = document.createElement('button');
+  button1.id = 'prevPage';
+  button1.classList.add('pageButton');
+  button1.innerHTML = 'Voltar';
+  button2.id = 'nextPage';
+  button2.classList.add('pageButton');
+  button2.innerHTML = 'Avançar';
+  pageControls.appendChild(button1);
+  pageControls.appendChild(button2);
+};
 
-window.onload = () => {
-  pageLeft.textContent = 'Hello mundo!';
-  pageRight.textContent = 'Ola World!';
-  generateButtons()
-}
+const generateActButtons = (inner1, inner2) => {
+  const button3 = document.createElement('button');
+  const button4 = document.createElement('button');
+  button3.id = 'firstAction';
+  button3.classList.add('actionButton');
+  button3.innerHTML = inner1;
+  button4.id = 'secondAction';
+  button4.classList.add('actionButton');
+  button4.innerHTML = inner2;
+  actionControls.appendChild(button3);
+  actionControls.appendChild(button4);
+};
+generatePageButtons();
+// generateActButtons('Bater', 'Defender');
+const savePage = () => {
+  const leftContent = lastLeftPage;
+  const rightContent = lastRightPage;
+  localStorage.setItem('pageLeftNumber', leftContent);
+  localStorage.setItem('pageRightNumber', rightContent);
+};  
+
+const pageC = document.getElementsByClassName('pageControls');
+for (const button of pageC) {
+  button.addEventListener('click', savePage);
+};
