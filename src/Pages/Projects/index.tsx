4b967a5aa2f15c1232ad2054a830ee8e20313eea
Victor Ciolac <victor.bcfraga@gmail.com>
2024-04-25 19:29:39 -0300

    atualizando projetos

--- src/Pages/Projects/index.tsx
+++ src/Pages/Projects/index.tsx
@@ -22,9 +22,30 @@ import pid0 from '../../assets/images/pid0.jpeg';
 import pid1 from '../../assets/images/pid1.jpeg';
 import pid2 from '../../assets/images/pid2.jpeg';
 import pid3 from '../../assets/images/pid3.jpeg';
-
+import lentes from '../../assets/images/lentes.png';
+import lentePhone from '../../assets/images/lente-phone.png';
+import tora1 from '../../assets/images/tora-1.png';
+import tora2 from '../../assets/images/tora-2.png';
+import tora3 from '../../assets/images/tora-3.png';
+import toraMobile from '../../assets/images/tora-mobile.png';
 
 const projects = [
+  {
+    name: "E-commerce - Loja de Biojoias",
+    description: `E-commerce fullstack para a marca de biojoias, Tora Wood.
+    Este site oferece uma experiência de compra completa, desde a navegação intuitiva até o checkout seguro e eficiente. 
+    Com recursos avançados como integração de pagamento via Pix e cartões de crédito, e-mails automáticos de confirmação e atualizações de pedido, além de autenticação JWT para garantir a segurança. 
+    Utilizando MongoDB como banco de dados, o site também oferece funcionalidades como avaliação de usuários, recomendações personalizadas e muito mais. É a fusão perfeita entre elegância e tecnologia, trazendo a beleza das biojoias da Tora Wood para o mundo online.`,
+    images: [
+      tora1,
+      toraMobile,
+      tora2,
+      tora3,
+    ],
+    links: {
+      deploy: "https://torawood.vercel.app/",
+    },
+  },
   {
     name: "Jogo Pigeon Adventure",
     description: `Jogo desenvolvido tanto para desktop quanto para celular, onde o jogador controla um pombo durante sua jornada.
@@ -42,8 +63,36 @@ const projects = [
     },
   },
   {
+    name: "Lentes e Fotografia",
+    description: `Este projeto é uma plataforma online onde fotógrafos podem exibir seu trabalho de forma profissional. 
+    Os usuários podem navegar por diferentes álbuns, favoritar e avaliar as imagens. 
+    Os administradores têm acesso a um painel para fazer upload de novas fotos e gerenciar o conteúdo. 
+    Desenvolvido em fullstack, o projeto utiliza o Firebase Cloud para armazenar as imagens na nuvem.`,
+    images: [
+      lentes,
+      lentePhone,
+    ],
+    links: {
+      github: "https://github.com/vCiolac/lentes-e-fotografia",
+      deploy: "https://lentesfotografia.vercel.app/",
+    },
+  },
+  {
+    name: "Site Pessoal - Portfólio",
+    description: `Meu incrível site pessoal, inspirado no Visual Studio Code, onde você está agora!!
+    Explore todas as funções disponíveis, como navegação e movimento das abas, mudanças de temas, visualização em código e muito mais!`,
+    images: [
+      Portf,
+      PortfPhone,
+    ],
+    links: {
+      github: "https://github.com/vCiolac/vCiolac.github.io/tree/main/port-vscode",
+      deploy: "https://vciolac.vercel.app/",
+    },
+  },
+  {
     name: "Jogo Choose Your Own - Aventura em Texto",
-    description: `Meu primeiro projeto de jogo, ele se baseia em uma aventura por texto desenvolvido exclusivamente em JavaScript.'
+    description: `Meu primeiro projeto de jogo, ele se baseia em uma aventura por texto desenvolvido exclusivamente em JavaScript.
     'O jogo oferece aos jogadores a oportunidade de tomar decisões que influenciam diretamente o rumo da história. 
       Cada escolha leva a diferentes desfechos, junto de novas imagens e sons tornando a experiência altamente interativa!`,
     images: [
@@ -53,18 +102,6 @@ const projects = [
     links: {
       github: "https://github.com/vCiolac/vCiolac.github.io/tree/main/cyo",
       deploy: "https://vciolac.github.io/cyo/",
-    },
-  },
-  {
-    name: "Site Pessoal - Portfólio",
-    description: `Meu incrível site pessoal, inspirado no Visual Studio Code, onde você está agora!!`,
-    images: [
-      Portf,
-      PortfPhone,
-    ],
-    links: {
-      github: "https://github.com/vCiolac/vCiolac.github.io/tree/main/port-vscode",
-      deploy: "https://vciolac.vercel.app/",
     },
   },
   {
@@ -158,7 +195,7 @@ function Projects() {
       <Typography sx={{ color: "tan", fontFamily: "Jost", ml: 2.5 }} variant="overline">
         Meus projetos
       </Typography>
-      <Grid container spacing={1}>
+      <Grid container spacing={1} sx={{ justifyContent: "center" }}>
         {projects.map((project, i) => (
           <Grid item xs={12} sm={8} md={4} key={i}>
             <Card sx={!isMobile ? styles.cardContainer : styles.phoneCards}>
@@ -198,7 +235,7 @@ function Projects() {
                   Deploy
                 </Button>}
 
-                <Button
+                {project.links.github && <Button
                   size="small"
                   color="primary"
                   component="a"
@@ -206,7 +243,7 @@ function Projects() {
                   target="_blank"
                 >
                   Repositório Github
-                </Button>
+                </Button>}
               </CardActions>
             </Card>
           </Grid>
